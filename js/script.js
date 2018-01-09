f785156446f63e91b1b78b8ac8839d79766e38ee
Szymon Pajzert <szymonpajzert@gmail.com>
2018-01-09 13:05:23 +0100

    Moving around

--- js/script.js
+++ js/script.js
@@ -1,60 +1,81 @@
 var app = angular.module('myApp', []);
 
-app.directive('shortcut', function() {
-  return {
-    restrict: 'E',
-    replace: true,
-    scope: true,
-    link: function postLink(scope, iElement, iAttrs){
-      $(document)
-        .on('keypressed', function(e){scope.$apply(scope.keyDown(e));})
-        .on('keyup', function(e){scope.$apply(scope.keyUp(e));});
-    }
-  };
+app.directive('shortcut', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: true,
+        link: function postLink(scope, iElement, iAttrs) {
+            $(document)
+                .on('keypressed', function (e) {
+                    scope.$apply(scope.keyDown(e));
+                })
+                .on('keyup', function (e) {
+                    scope.$apply(scope.keyUp(e));
+                });
+        }
+    };
 });
 
-function GreetingController($scope, $document) {
-  log("hehe");
+function GreetingController($scope, $http) {
+    console.log("hehe");
 
-  $scope.pressed = [false, false, false, false, false, false];
+    $scope.pressed = [false, false, false, false, false, false];
 
-  $scope.keyDown = function(event) {
-    var key = toInt(event);
-    if(key != null) {
-      $scope.pressed[key] = true;
-    }
-    console.log($scope.pressed);
-  };
+    $scope.keyDown = function (event) {
+        var key = toInt(event);
+        if (key != null) {
+            $scope.pressed[key] = true;
+        }
 
-  $scope.keyUp = function(event) {
-    var key = toInt(event);
-    if(key != null) {
-      $scope.pressed[key] = false;
-    }
-  };
+        console.log('hifhidfi');
+        var file = new Audio(window.location.href + 'static/Buzz-SoundBible.com-1790490578.mp3');
+        file.play();
+    };
 
-  $scope.ask = function(item) {
-    return $scope.pressed[item];
-  };
+    $scope.keyUp = function (event) {
+        var key = toInt(event);
+        if (key != null) {
+            $scope.pressed[key] = false;
+        }
+    };
 
-  $(document)
-    .bind('keydown', function(e){$scope.$apply($scope.keyDown(e));})
-    .bind('keyup', function(e){$scope.$apply($scope.keyUp(e));});
-};
+    $scope.ask = function (item) {
+        return $scope.pressed[item];
+    };
 
-GreetingController.$inject = ['$scope'];
+    $(document)
+        .bind('keydown', function (e) {
+            $scope.$apply($scope.keyDown(e));
+        })
+        .bind('keyup', function (e) {
+            $scope.$apply($scope.keyUp(e));
+        });
+
+    $http.get(window.location + '/config').success(function (results) {
+        console.log(results);
+        $scope.delay = results.delay;
+        $scope.feedback = results.feedback;
+    }).error(function (error) {
+        console.log(error);
+    });
+
+
+}
+
+GreetingController.$inject = ['$scope', '$http'];
 
 app.controller("GreetingController", GreetingController);
 
 function toInt(event) {
-  if(!isNaN(event.key)) {
-    if(event.key == "0") {
-      return parseInt(event.key) + 9;
+    if (!isNaN(event.key)) {
+        if (event.key == "0") {
+            return parseInt(event.key) + 9;
+        } else {
+            return parseInt(event.key) - 1;
+        }
     } else {
-      return parseInt(event.key) - 1;
+        return null;
     }
-  } else {
-    return null;
-  }
 }
 
--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 from flask import Flask
 from flask import render_template, send_from_directory
-
+from flask import jsonify
 
 app = Flask(__name__, static_url_path='')
+
+from config import Config
+
+application_config = {
+    0: Config(True, 100)
+}
+
 
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
+
 
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('css', path)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+
+@app.route('/new/')
+def create_new():
+    pass
+
+
+@app.route('/admin/<int:id>')
+def manage_experiment(id):
+    pass
+
+
+@app.route('/admin/')
+def admin_panel():
+    return render_template('admin.html')
+
+
+@app.route('/experiment/<int:id>')
+def hello_world(id):
     return render_template('index.html')
+
+
+@app.route('/experiment/<int:id>/config')
+def experiment_config(id):
+    return jsonify(application_config[id].dict())
+
+app.run(host='0.0.0.0')
